df425a02d4b8a61c9de0bdb4ccb7262b63dc5b8f
Loc Nguyen <h_nguyen128@u.pacific.edu>
2023-10-19 17:48:13 -0700

    added a new package "parser" for project phase 2.1

---
+++ toy_interpreter/src/Parser/parser.java
@@ -0,0 +1,16 @@
+package Parser;
+
+import scannerPhase2.ScannerPhase2;
+
+public class parser {
+
+	public parser() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
