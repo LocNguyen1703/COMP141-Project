691e935345cc98f81be18d0efbfabd065a59c303
Loc Nguyen <h_nguyen128@u.pacific.edu>
2023-10-20 12:38:25 -0700

    roughly created a class Tree to represent Abstract Syntax Tree (we have left, right, mid and the value of node itself). don't know how to actually create an AST yet

--- toy_interpreter/src/Parser/parser.java
+++ toy_interpreter/src/Parser/parser.java
@@ -8,10 +8,29 @@ import scannerPhase2.ScannerPhase2.Token;
 
 public class parser {
 	
-	
 	public parser() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public class TreeNode {
+		public TreeNode left; 
+		public TreeNode mid;
+		public TreeNode right; 
+		Token token;
+	
+		public TreeNode(Token token) {
+			this.token = token;
+		}
+	}
+
+	public class Tree {
+		public TreeNode root;
+	}
+	
+	public void createTree(List<List<Token>> tokens) {
+		
+	}
+	
 	
 	public static void AST(List<List<Token>> tokens) {
 		
