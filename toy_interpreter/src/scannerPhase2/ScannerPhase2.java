4b1f6f1aa031f2b7c0dc014f06f1550d8d379973
Loc Nguyen <h_nguyen128@u.pacific.edu>
2023-10-19 21:29:37 -0700

    scannerphase2 fixed - it now implements principle of longest substring

--- toy_interpreter/src/scannerPhase2/ScannerPhase2.java
+++ toy_interpreter/src/scannerPhase2/ScannerPhase2.java
@@ -24,46 +24,114 @@ public class ScannerPhase2 {
 	
 	public static List<List<Token>> tokenizeFile(String inputFile) throws IOException {
 		List<List<Token>> result_tokens = new ArrayList<>();
-		Pattern identifier = Pattern.compile("([a-zA-Z])([a-zA-Z0-9])*");
-		Pattern num = Pattern.compile("[0-9]+");
-		Pattern symbol = Pattern.compile("\\+|\\-|\\*|/|\\(|\\)|\\:=|\\;");
-		Pattern keyword = Pattern.compile("\\b(if|then|else|endif|while|do|endwhile|skip)\\b");
-		Pattern error = Pattern.compile("&|#|\\.|\\,|\\]|\\[");
+//		Pattern identifier = Pattern.compile("([a-zA-Z])([a-zA-Z0-9])*");
+//		Pattern num = Pattern.compile("[0-9]+");
+//		Pattern symbol = Pattern.compile("\\+|\\-|\\*|/|\\(|\\)|\\:=|\\;");
+//		Pattern keyword = Pattern.compile("\\b(if|then|else|endif|while|do|endwhile|skip)\\b");
+//		Pattern error = Pattern.compile("&|#|\\.|\\,|\\]|\\[");
+		
+		String identifier = "([a-zA-Z])([a-zA-Z0-9])*";
+		String num = "[0-9]+";
+		String symbol = "\\+|\\-|\\*|/|\\(|\\)|\\:=|\\;";
+		String keyword = "\\b(if|then|else|endif|while|do|endwhile|skip)\\b";
+		String error = "&|#|\\.|\\,|\\]|\\[";
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
 			String line;
 			while ((line = br.readLine()) != null) {
 				List<Token> separated_result_tokens = new ArrayList<>();
-		        for (String i : line.split(" ")) {
-		            Matcher match_identifier = identifier.matcher(i);
-		            Matcher match_num = num.matcher(i);
-		            Matcher match_symbol = symbol.matcher(i);
-		            Matcher match_keyword = keyword.matcher(i);
-		            Matcher match_error = error.matcher(i);
-		            
-		            if (match_keyword.matches()) separated_result_tokens.add(new Token(TokenType.KEYWORD, i));
-		            else if (match_num.matches()) separated_result_tokens.add(new Token(TokenType.NUMBER, i));
-		            else if (match_symbol.matches()) separated_result_tokens.add(new Token(TokenType.SYMBOL, i));
-		            else if (match_identifier.matches()) separated_result_tokens.add(new Token(TokenType.IDENTIFIER,i));
-		            //
-		            else {
-		            	while (match_identifier.find()) {
-		            		separated_result_tokens.add(new Token(TokenType.IDENTIFIER, match_identifier.group()));
-		                }
-		                while (match_num.find()) {
-		                	separated_result_tokens.add(new Token(TokenType.NUMBER, match_num.group()));
-		                }
-		                while (match_symbol.find()) {
-		                	separated_result_tokens.add(new Token(TokenType.SYMBOL, match_symbol.group()));
-		                }
-		                while (match_keyword.find()) {
-		                	separated_result_tokens.add(new Token(TokenType.KEYWORD, match_keyword.group()));
-		                }
-		                while (match_error.find()) {
-		                	separated_result_tokens.add(new Token(TokenType.ERROR, match_error.group()));
-			            }	
-		            }	            	
-		        }
+//		        for (String i : line.split(" ")) {
+//		            Matcher match_identifier = identifier.matcher(i);
+//		            Matcher match_num = num.matcher(i);
+//		            Matcher match_symbol = symbol.matcher(i);
+//		            Matcher match_keyword = keyword.matcher(i);
+//		            Matcher match_error = error.matcher(i);
+//		            
+//		            if (match_keyword.matches()) separated_result_tokens.add(new Token(TokenType.KEYWORD, i));
+//		            else if (match_num.matches()) separated_result_tokens.add(new Token(TokenType.NUMBER, i));
+//		            else if (match_symbol.matches()) separated_result_tokens.add(new Token(TokenType.SYMBOL, i));
+//		            else if (match_identifier.matches()) separated_result_tokens.add(new Token(TokenType.IDENTIFIER,i));
+//		            //
+//		            else {
+//		            	while (match_identifier.find()) {
+//		            		separated_result_tokens.add(new Token(TokenType.IDENTIFIER, match_identifier.group()));
+//		                }
+//		                while (match_num.find()) {
+//		                	separated_result_tokens.add(new Token(TokenType.NUMBER, match_num.group()));
+//		                }
+//		                while (match_symbol.find()) {
+//		                	separated_result_tokens.add(new Token(TokenType.SYMBOL, match_symbol.group()));
+//		                }
+//		                while (match_keyword.find()) {
+//		                	separated_result_tokens.add(new Token(TokenType.KEYWORD, match_keyword.group()));
+//		                }
+//		                while (match_error.find()) {
+//		                	separated_result_tokens.add(new Token(TokenType.ERROR, match_error.group()));
+//			            }	
+//		            }	            	
+//		        }
+				String longest_substring = "";
+				int iter = 0;
+				
+				while (iter < line.length()) {
+					longest_substring += line.charAt(iter);
+					if (longest_substring.matches(num)) {
+						while (iter + 1 < line.length() && (longest_substring + line.charAt(iter+1)).matches(num)) {
+							iter ++;
+							longest_substring += line.charAt(iter);
+						}
+//						System.out.println(longest_substring + ": NUMBER");
+						separated_result_tokens.add(new Token(TokenType.NUMBER, longest_substring));
+						longest_substring = ""; 
+					}
+					
+					else if (longest_substring.matches(keyword)) {
+						while (iter + 1 < line.length() && (longest_substring + line.charAt(iter+1)).matches(keyword)) {
+							iter ++;
+							longest_substring += line.charAt(iter);
+						}
+//						System.out.println(longest_substring + ": ERROR");
+						separated_result_tokens.add(new Token(TokenType.KEYWORD, longest_substring));
+						longest_substring = ""; 
+					}
+					
+					else if (longest_substring.matches(identifier)) {
+						while (iter + 1 < line.length() && (longest_substring + line.charAt(iter+1)).matches(identifier)) {
+							iter ++;
+							longest_substring += line.charAt(iter);
+						}
+						if (longest_substring.matches(keyword)) {
+							separated_result_tokens.add(new Token(TokenType.KEYWORD, longest_substring));
+						}
+//						System.out.println(longest_substring + ": IDENTIFIER");
+						else separated_result_tokens.add(new Token(TokenType.IDENTIFIER, longest_substring));
+						longest_substring = ""; 
+					}
+					
+					else if (longest_substring.matches(symbol)) {
+						while (iter + 1 < line.length() && (longest_substring + line.charAt(iter+1)).matches(symbol)) {
+							iter ++;
+							longest_substring += line.charAt(iter);
+						}
+//						System.out.println(longest_substring + ": SYMBOL");
+						separated_result_tokens.add(new Token(TokenType.SYMBOL, longest_substring));
+						longest_substring = ""; 
+					}
+					
+					else if (longest_substring.matches(error)) {
+						while (iter + 1 < line.length() && (longest_substring + line.charAt(iter+1)).matches(error)) {
+							iter ++;
+							longest_substring += line.charAt(iter);
+						}
+//						System.out.println(longest_substring + ": ERROR");
+						separated_result_tokens.add(new Token(TokenType.ERROR, longest_substring));
+						longest_substring = ""; 
+					}
+					
+					longest_substring = "";
+					iter++;
+				}
+				
 		        result_tokens.add(separated_result_tokens);
 			}
 		}
